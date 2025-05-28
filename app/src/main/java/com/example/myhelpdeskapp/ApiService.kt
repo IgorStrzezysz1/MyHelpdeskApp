52fbb9bf3112c508990b11f16554c1727733d285
IgorStrzezysz1 <93446017+IgorStrzezysz1@users.noreply.github.com>
2025-05-28 18:18:53 +0200

    Nowe poprawki kodu v2

--- app/src/main/java/com/example/myhelpdeskapp/ApiService.kt
+++ app/src/main/java/com/example/myhelpdeskapp/ApiService.kt
@@ -1,10 +1,14 @@
 package com.example.myhelpdeskapp
-
+import android.provider.ContactsContract.CommonDataKinds.Email
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("/api/v1/register")
     fun registerUser(@Body user: User): Call<Void>
+    @POST("/api/v1/login")
+    fun login(@Header("X-Auth-Email")
+              email: String,@Header("X-Auth-Password")password: String): Call<AuthResponse>
 }
---
+++ app/src/main/java/com/example/myhelpdeskapp/AuthResponse.kt
@@ -0,0 +1,5 @@
+package com.example.myhelpdeskapp
+
+data class AuthResponse(
+    val token: String
+)
